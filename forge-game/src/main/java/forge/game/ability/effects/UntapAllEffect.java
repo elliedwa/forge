14205e06a9366346a779ae37d5e53f64a17cf675
Northmoc <timothy.s.mocny@gmail.com>
2023-11-04 20:30:28 -0400

    LCI: the_millennium_calendar.txt and support (TriggerUntapAll)

--- forge-game/src/main/java/forge/game/ability/effects/UntapAllEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/UntapAllEffect.java
@@ -1,12 +1,18 @@
 package forge.game.ability.effects;
 
+import com.google.common.collect.Maps;
+import forge.game.ability.AbilityKey;
 import forge.game.ability.SpellAbilityEffect;
 import forge.game.card.Card;
-import forge.game.card.CardCollectionView;
+import forge.game.card.CardCollection;
 import forge.game.card.CardLists;
+import forge.game.player.Player;
 import forge.game.spellability.AbilitySub;
 import forge.game.spellability.SpellAbility;
+import forge.game.trigger.TriggerType;
 import forge.game.zone.ZoneType;
+
+import java.util.Map;
 
 public class UntapAllEffect extends SpellAbilityEffect {
     @Override
@@ -20,22 +26,23 @@ public class UntapAllEffect extends SpellAbilityEffect {
     @Override
     public void resolve(SpellAbility sa) {
         final Card card = sa.getHostCard();
-        CardCollectionView list;
+        final Player activator = sa.getActivatingPlayer();
         final String valid = sa.getParamOrDefault("ValidCards", "");
+        Map<Player, CardCollection> untapMap = Maps.newHashMap();
 
-        if (!sa.usesTargeting() && !sa.hasParam("Defined")) {
-            list = sa.getActivatingPlayer().getGame().getCardsIn(ZoneType.Battlefield);
-        } else {
-            list = getTargetPlayers(sa).getCardsIn(ZoneType.Battlefield);
-        }
-        list = CardLists.getValidCards(list, valid, sa.getActivatingPlayer(), card, sa);
-
-        boolean remember = sa.hasParam("RememberUntapped");
-        for (Card c : list) {
-            c.untap(true);
-            if (remember) {
-                card.addRemembered(c);
+        for (Player p : getTargetPlayers(sa)) {
+            for (Card c : CardLists.getValidCards(p.getCardsIn(ZoneType.Battlefield), valid, activator, card, sa)) {
+                if (c.untap(true))  {
+                    untapMap.computeIfAbsent(p, i -> new CardCollection()).add(c);
+                    if (sa.hasParam("RememberUntapped")) card.addRemembered(c);
+                }
             }
+        }
+
+        if (!untapMap.isEmpty()) {
+            final Map<AbilityKey, Object> runParams = AbilityKey.newMap();
+            runParams.put(AbilityKey.Map, untapMap);
+            activator.getGame().getTriggerHandler().runTrigger(TriggerType.UntapAll, runParams, false);
         }
     }
 }
